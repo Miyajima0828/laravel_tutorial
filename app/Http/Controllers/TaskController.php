0e4fabebd0c37b5ce7daccf06a76d255db995e60
Miyajima0828 <miyajima08281234@gmail.com>
2024-05-12 23:09:57 +0900

    [update]タスクを作成できるメソッドを追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTask;
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Contracts\View\View;
 
 class TaskController extends Controller
@@ -19,10 +21,32 @@ class TaskController extends Controller
         // 選ばれたフォルダに紐づくタスクを取得する
         $tasks = $current_folder->tasks()->get();
 
-        return view('tasks.index',[
+        return view('tasks.index', [
             'folders' => $folders,
             'current_folder_id' => $current_folder->id,
             'tasks' => $tasks,
+        ]);
+    }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
