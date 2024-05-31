98070faabb4b13e202a82c06d072625ca08537bb
Miyajima0828 <miyajima08281234@gmail.com>
2024-05-31 18:33:47 +0900

    [update]foldersテーブルとusersテーブルをリレーションをしたことによるテストの修正およびルーティングの修正

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Carbon\Carbon;
 use Tests\TestCase;
@@ -9,30 +10,29 @@ use Tests\TestCase;
 class TaskTest extends TestCase
 {
     use RefreshDatabase;
-
+    private User $user;
     /**
      * 各テストメソッドの実行前に呼ばれる
      */
     public function setUp(): void
     {
         parent::setUp();
-
+        $this->seed('UsersTableSeeder');
+        $this->user = User::first();
         // テストケース実行前にフォルダデータを作成する
         $this->seed('FoldersTableSeeder');
     }
     /**
-     * 期限日が日付ではない場合はバリデーションエラー
      * @test
      */
-    public function due_date_should_be_date()
+    public function 期限日が日付であること()
     {
-        $response = $this->post('/folders/1/tasks/create', [
+        $response = $this->actingAs($this->user)->post('/folders/1/tasks/create', [
             'title' => 'Sample task',
             'due_date' => 123, // 不正なデータ（数値）
         ]);
-
         $response->assertSessionHasErrors([
-            'due_date' => '期限日 には日付を入力してください。',
+            'due_date' => '期限日には有効な日付を指定してください。',
         ]);
     }
 
@@ -42,13 +42,33 @@ class TaskTest extends TestCase
      */
     public function due_date_should_not_be_past()
     {
-        $response = $this->post('/folders/1/tasks/create', [
+        $response = $this->actingAs($this->user)->post('/folders/1/tasks/create', [
             'title' => 'Sample task',
             'due_date' => Carbon::yesterday()->format('Y/m/d'), // 不正なデータ（昨日の日付）
         ]);
 
         $response->assertSessionHasErrors([
             'due_date' => '期限日 には今日以降の日付を入力してください。',
+        ]);
+    }
+
+    /**
+     * 状態が定義された値ではない場合はバリデーションエラー
+     * @test
+     */
+    public function status_should_be_within_defined_numbers()
+    {
+
+        $this->seed('TasksTableSeeder');
+
+        $response = $this->actingAs($this->user)->post('/folders/1/tasks/1/edit', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 999,
+        ]);
+
+        $response->assertSessionHasErrors([
+            'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
         ]);
     }
 }
