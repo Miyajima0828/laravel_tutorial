80335496e1f3034858e8d9f92d67d7ae783a3e33
Miyajima0828 <miyajima08281234@gmail.com>
2024-05-31 15:38:41 +0900

    [update]フォルダを追加する際にuserIdに紐づくように修正

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateFolder;
 use App\Models\Folder;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Contracts\View\View;
 
 class FolderController extends Controller
 {
+    public function __construct(
+        private AuthManager $authManager
+    ) {
+    }
     public function showCreateForm(): View
     {
         return view('folders/create');
@@ -19,6 +24,7 @@ class FolderController extends Controller
         $folder = new Folder();
         // タイトルに入力値を代入する
         $folder->title = $request->title;
+        $folder->user_id = $this->authManager->guard()->user()->id;
         // インスタンスの状態をデータベースに書き込む
         $folder->save();
 
