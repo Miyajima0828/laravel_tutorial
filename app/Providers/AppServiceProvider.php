7c226cadcf771c7c5a7ebe7baae379d5ae3bac38
Miyajima0828 <miyajima08281234@gmail.com>
2024-06-14 18:13:17 +0900

    [update]フォルダの閲覧を制限するためのポリシークラスとモデルを紐付けるための記述を追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Folder;
+use App\Policies\FolderPolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Folder::class => FolderPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
