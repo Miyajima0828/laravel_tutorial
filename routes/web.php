16682c62345bfa46508824bf1c4666b589ae091a
Miyajima0828 <miyajima08281234@gmail.com>
2024-06-14 18:16:10 +0900

    [update]ルーティングにポリシークラスを反映させるための修正

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,25 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
     Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
     Route::post('/folders/create', [FolderController::class, 'create']);
 
-    Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
-    Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
+    Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+    Route::post('/folders/{folder}/tasks/create', [TaskController::class, 'create']);
 
-    Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-    Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+    Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+    Route::post('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit']);
+
+    Route::middleware(['can:view,folder'])->group(function () {
+        Route::get('/folders/{folder}/tasks',[TaskController::class,'index'])->name('tasks.index');
+        Route::get('/folders/{folder}/tasks/create', [TaskController::class,'showCreateForm'])->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', [TaskController::class,'create']);
+
+        Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class,'showEditForm'])->name('taksk.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', [TaskController::class,'edit']);
+    });
 });
 
 require __DIR__ . '/auth.php';
