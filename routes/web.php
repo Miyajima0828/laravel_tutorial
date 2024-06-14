7cfa7b714460f7e47fae4f932d8191313b538a06
Miyajima0828 <miyajima08281234@gmail.com>
2024-06-14 14:32:25 +0900

    [refactor]ルーティングにおいて、認証が必要なルーティングはauthミドウェアのグループでまとめるように修正

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
