9d0c65490b61c16ab4801c28e323edbc1b0a455b
Miyajima0828 <miyajima08281234@gmail.com>
2024-06-14 14:30:49 +0900

    [update]パスワードリセットできるようにするための修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Mail\ResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Folder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Mail;
 
+/**
+ * @property array<int, string> $fillable
+ * @property array<int, string> $hidden
+ * @property array<int, string> $casts
+ * @property Folder[] $folders
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -43,8 +54,16 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function folders()
+    /**
+     * @return hasMany
+     */
+    public function folders(): HasMany
     {
         return $this->hasMany(Folder::class);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        Mail::to($this)->send(new ResetPassword($token));
     }
 }
